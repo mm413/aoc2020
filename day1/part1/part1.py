1371a19d67ac9a63f4e395da47b74eeb7adbcc7f
mm413 <mm365473@wne.edu>
2020-12-01 01:17:07 -0800

    finished part 1

---
+++ day1/part1/part1.py
@@ -0,0 +1,28 @@
+# day1 part 1
+import os
+def main():
+    file = open("C:/Users/Mark/Desktop/aoc2020/day1/part1/input.txt", "r")
+    numbers = []
+    for line in file:
+        numbers.append(line.strip())
+    check(numbers)
+
+
+
+
+def check(numbers):
+    for i in range(len(numbers)):
+            # if i==0:
+            #     pass
+            if int(numbers[0]) + int(numbers[i]) == 2020:
+                print(int(numbers[0]) * int(numbers[i])
+                return
+    print(len(numbers))
+    numbers.pop(0)
+    check(numbers)
+    
+
+
+main()
+
+
